7b5797daa8101321902e17e789b3f9e61cd7155e
Killjoy <aryaajitnair@gmail.com>
2021-12-02 23:05:15 +0530

    Simple Feature Scaling

--- module_2.2_Data_Normalisation.py
+++ module_2.2_Data_Normalisation.py
@@ -1,2 +1,19 @@
 import pandas as pd
 
+#To read the data from the csv file
+df=pd.read_csv("corrected_data3.csv")
+
+'''
+    Normalisation is the process of transforming the data into a range of values between 0 and 1.
+    This is done to make the data easier to process and easier to compare.  
+    There are 3 main ways to normalise the data
+    1) Min-Max normalisation
+        x' = (x - min(x)) / (max(x) - min(x))
+    2) Z-score normalisation
+        x' = (x - mean(x)) / std(x)
+    3) Simple feature scaling
+        x' = x / max(x)
+'''
+#Simple feature scaling
+df["length"]=df["length"]/df["length"].max()
+print(df["length"])
