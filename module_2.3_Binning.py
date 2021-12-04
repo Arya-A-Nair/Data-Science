2260d0fc6a56048156b2a1e03921951906b93ccb
Killjoy <aryaajitnair@gmail.com>
2021-12-04 23:56:24 +0530

    Binning

---
+++ module_2.3_Binning.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import numpy as np
+
+df=pd.read_csv("corrected_data.csv")
+df.dropna(axis = 1,inplace=True)
+df=df[df["price"]!='?']
+#print(df["price"])
+
+
+bins=np.linspace(min(df["price"]), max(df["price"]),4)
+
+group_names=["Low","Medium","High"]
+
+df["price-binned"]=pd.cut(df["price"],bins,labels=group_names, include_lowest=True)
+
+print (df["price-binned"])
+
