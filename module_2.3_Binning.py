14fb6211cfeb1841359becab897a7ae8b8ac247d
Killjoy <aryaajitnair@gmail.com>
2021-12-05 00:08:50 +0530

    Fixed the error due to dtype

--- module_2.3_Binning.py
+++ module_2.3_Binning.py
@@ -3,9 +3,8 @@ import numpy as np
 
 df=pd.read_csv("corrected_data.csv")
 df.dropna(axis = 1,inplace=True)
-df=df[df["price"]!='?']
+df['price'] = pd.to_numeric(df['price'], errors='coerce')
 #print(df["price"])
-
 
 bins=np.linspace(min(df["price"]), max(df["price"]),4)
 
@@ -13,5 +12,6 @@ group_names=["Low","Medium","High"]
 
 df["price-binned"]=pd.cut(df["price"],bins,labels=group_names, include_lowest=True)
 
-print (df["price-binned"])
+print(df["price-binned"])
 
+df["price-binned"].to_csv("price_binned.csv")
