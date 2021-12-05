34069244fd7dae96a75cf4c46fa78f56fb1de8ef
Killjoy <aryaajitnair@gmail.com>
2021-12-05 19:02:21 +0530

    Module 3 started

--- module_3_Descriptive_Statistics.py
+++ module_3_Descriptive_Statistics.py
@@ -1,2 +1,13 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
+df=pd.read_csv("corrected_data.csv")
+df.drop("Unnamed: 0",axis=1,inplace=True)
+
+df.describe()
+
+drive_wheel_counts=df["drive-wheels"].value_counts()
+
+
+
+df.describe()
