52de10ddfa44e802af163f9a1727a8ff9d1066b2
Killjoy <aryaajitnair@gmail.com>
2021-12-06 18:44:02 +0530

    Seaborn module error resolved

--- module_3_Descriptive_Statistics.py
+++ module_3_Descriptive_Statistics.py
@@ -1,13 +1,24 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 df=pd.read_csv("corrected_data.csv")
 df.drop("Unnamed: 0",axis=1,inplace=True)
 
 df.describe()
 
+#to get the number of FWD,RWD and 4WD cars
 drive_wheel_counts=df["drive-wheels"].value_counts()
 
 
+#boxplot method
+df['price'] = pd.to_numeric(df['price'], errors='coerce')
+sns.boxplot(x="drive-wheels",y="price",data=df)
 
-df.describe()
+#Scatter-Plot method
+y=df["engine-size"]
+x=df["price"]
+plt.scatter(x,y)
+plt.title("Engine Size vs Price")
+plt.ylabel("Engine-Size")
+plt.xlabel("Price")
